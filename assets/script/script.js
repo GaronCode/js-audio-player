cc91dde7e810dd78524edc6652062abb45496eab
Iliya <garon7777@ya.ru>
2024-09-22 15:44:01 +0300

    feat: finished layout

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,6 @@
 const audioPlayerContainer = document.getElementById("audio-player");
 
-audioPlayerContainer.addEventListener("click", () => {
+document.getElementById("audio-play-control").addEventListener("click", () => {
 	if (audioPlayerContainer.classList.contains("audio-player--play")) {
 		audioPlayerContainer.classList.add("audio-player--stop");
 		audioPlayerContainer.classList.remove("audio-player--play");
